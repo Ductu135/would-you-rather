4d0ef0ae0ad3a1d15d69402bd413ece34d679667
DucTu <55016705+Ductu135@users.noreply.github.com>
2022-11-28 02:26:37 +0700

    format code

--- src/index.js
+++ src/index.js
@@ -10,16 +10,14 @@ import { applyMiddleware } from "redux";
 import { legacy_createStore as createStore } from "redux";
 import { initialData } from "./utilities/InitialDataAPI";
 
-
 export const store = createStore(rootReducers, applyMiddleware(thunk));
 store.dispatch(initialData());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
   <Provider store={store}>
-      <App/>
-    </Provider>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
